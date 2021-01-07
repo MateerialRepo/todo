bad3ff999f5078d5d5684528244a3ef5480f6640
Oluyosola <elizabeth.afolabi@techadvance.ng>
2021-01-07 17:23:59 +0100

    Set up the registration backend(a user can now successfully register

--- database.php
+++ database.php
@@ -4,18 +4,18 @@ $username ="root";
 $password ="";
 $dbname = "todo";
 $conn = new mysqli($servername, $username, $password, $dbname);
-// if ($conn->connect_error){
-//     die ("connection failed" . $conn->connect_error);
-// }else{
-//     echo "connected successfully";
-// }
+if ($conn->connect_error){
+    die ("connection failed" . $conn->connect_error);
+}else{
+    echo "connected successfully";
+}
 // $sql = "CREATE DATABASE todo";
 // if($conn->query($sql) === TRUE){
 //     echo "database created successfully";
 // }else{
 //     echo "Database not craeted". $conn->error;
 // }
-// $sql = "CREATE TABLE registrations(
+// $sql = "CREATE TABLE users(
 //     id INT(6) AUTO_INCREMENT PRIMARY KEY,
 //     username VARCHAR(100) NOT NULL,
 //     email VARCHAR(100) NOT NULL,
@@ -25,5 +25,48 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 //     echo "Table created";
 // }else{
 //     echo "Table not craete" . $conn->error;
+// }
+
+// include('databse.php');
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "todo";
+// $conn = new mysqli($servername, $username, $password, $dbname);
+// $username ="";
+// $email ="";
+
+// if(isset($_POST['register'])){
+//     $username = $mysqli->real_escape_string($_POST['username']);
+//     $email = $mysqli->real_escape_string($_POST['email']);
+//     $password =$mysqli->real_escape_string($_POST['password']);
+//     $confirm_password = $mysqli->real_escape_string($_POST['confrim_password']);
+
+//     if(empty($username)){
+//         echo "Username is required";
+//     }
+//     if(empty($email)){
+//         echo "email is required";
+//     }
+//     if(empty($password)){
+//         echo "Password required";
+//     }
+//     if($password!=$confirm_password){
+//         echo "passwords don't match";
+
+//     }
+//     $results = mysqli_query($conn, "SELECT FROM users WHERE username = '$username' OR email = '$email' LIMIT 1") ;
+//     $user = mysqli_fetch_array($results);
+//     if($user){
+//         if($user['username'] === $username){
+//             echo "Username already exists";
+//         }
+//         if($user['email'] === $eamil){
+//             echo "email already exists";
+//         }
+//     };
+//     $password = sha1($password);
+//     $sql = mysqli_query($conn, "INSERT INTO users(username, email, password) VALUES ('$username', '$email', '$password')");
+//     echo "Registeration successful";
 // }
 ?>
--- todo.php
+++ todo.php
@@ -1,11 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>HELLO</h1>
-</body>
-</html>
+<?php
+// session_start();
+include('database.php');
+// include('error.php');
+// $username ="";
+$email = "";
+// $errors= "";
+if(isset($_POST['register'])){
+    $username =  mysqli_real_escape_string($conn, $_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+
+    if(empty($username)){
+        echo "Username is required";
+    }elseif(empty($email)){
+        echo "email is required";
+    }elseif(empty($password)){
+        echo "Password required";
+    }elseif($password != $confirm_password){
+        echo "passwords don't match";
+    }else{
+        $results = mysqli_query($conn, "SELECT * FROM users WHERE username = '$username' OR email = '$email' LIMIT 1") ;
+        $user = mysqli_fetch_array($results);
+            if($user['username'] === $username){
+                echo"Username already exists";
+            }
+            elseif($user['email'] === $email){
+                echo "email already exists";
+             }else{
+                $password = sha1($password);
+                $sql = mysqli_query($conn, "INSERT INTO users(username, email, password) VALUES ('$username', '$email', '$password')");
+                echo "successful";
+             }
+    // $_SESSION['username'] = $username;
+//   	$_SESSION['success'] = "You are now logged in";
+ 
+    }
+}   
+?>
